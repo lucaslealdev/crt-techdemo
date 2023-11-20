27f314fa4cc1b8ab1851a51f124ad4349df2a12d
Lucas Leal <lucas.leal@betrybe.com>
2023-11-20 15:28:05 -0300

    uso com playlist do youtube

--- script.js
+++
@@ -1,9 +0,0 @@
-video.addEventListener('play', () => {
-	reflexo.play();
-});
-video.addEventListener('pause', () => {
-	reflexo.pause();
-});
-video.addEventListener('timeupdate', () => {
-	reflexo.currentTime = video.currentTime;
-});

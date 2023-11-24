d910c971665f2c545dcd207d5d19a3d39caa4fa0
Lucas Leal <lucas.leal@betrybe.com>
2023-11-24 12:52:24 -0300

    adiciona ruido branco

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 video.addEventListener('play', () => {
 	reflexo.play();
+  whiteNoise.play();
 });
 video.addEventListener('pause', () => {
 	reflexo.pause();
+  whiteNoise.pause();
 });
 video.addEventListener('timeupdate', () => {
 	reflexo.currentTime = video.currentTime;
